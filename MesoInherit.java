ac8c4e29d20d62128679b87e8b973c38ab07bab4
AnthonyBalch <Anthonybalch27@ou.edu>
2019-10-06 13:51:40 -0500

    added MesoInherit.java and PosAvg.java

---
+++ MesoInherit.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+
+public class MesoInherit {
+	
+	
+	//calAverage Override
+	@Override
+	public int[] calAverage() {
+		
+	}
+	
+	//letterAverage Override
+	@Override
+	char letterAverage() {
+		
+	}
+}
