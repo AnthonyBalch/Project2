3e8b4f6c71003174abf9680dfdd09225880264cb
AnthonyBalch <Anthonybalch27@ou.edu>
2019-10-07 20:57:41 -0500

    changed formatting

--- PosAvg.java
+++ PosAvg.java
@@ -37,83 +37,69 @@ public class PosAvg {
 		
 	}
 	//try to read file method
-		public void readFile() {
-			String filename = "Mesonet.txt";
-			
-			// Try to read file. Throws IOException if fails.
-			try {
-				read(filename); // IMPLEMENT READ
-			}
-			catch(IOException e) {
-				System.out.println("Error reading from file! \n");
-				e.printStackTrace();
-			}
+	public void readFile() {
+		String filename = "Mesonet.txt";
+		// Try to read file. Throws IOException if fails.
+		try {
+			read(filename); // IMPLEMENT READ
 		}
-		//read the actual file into an array method
-		public void read(String filename) throws IOException {
-			BufferedReader br = new BufferedReader(new FileReader(filename));
-			String strg = null;
-			String firstElement = "AAAA";  // String used to find "STID" in text
-			String index = "AAAA";         // String that will be the station ID
-			int indexCount = 0;            // Counter for array.
-			String[] strgArray = new String[5]; //readLine array
-			
-			// look for beginning of data, search for "STID"
-			while(!firstElement.contentEquals(STID_COLUMN)) {
-			strg = br.readLine();
-			strgArray = strg.split(" ");
-			firstElement = strgArray[1];
-			// System.out.println("The first element in: " + firstElement); // Show me whats stored HELPER
-			}
-			
-			// Once STID is found, we can begin reading in the strings into an. array.
-			// We only want the 1st token of the string, the STID.
-			// while loops until the finalString is reached.
-			while (!index.contentEquals(finalstring)) {
-			strg = br.readLine();
-			strgArray = strg.split(" ");
-			index = strgArray[1];
-			stations[indexCount] = index;
-			// System.out.println(stations[indexCount]);            // Show me whats printed HELPER
-			indexCount++;	
-			
-			// creates an array +1 bigger than the original, when limit is reached.
-			if (indexCount == stations.length) {
-			String[] tempArray = new String[indexCount+1];
-			for (int i = 0; i < stations.length; i++) {
-				tempArray[i] = stations[i];
-				}
-			stations = tempArray;
-			} 
-			
+		catch(IOException e) {
+			System.out.println("Error reading from file! \n");
+			e.printStackTrace();
+		}
+	}
+	//read the actual file into an array method
+	public void read(String filename) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		String strg = null;
+		String firstElement = "AAAA";  // String used to find "STID" in text
+		String index = "AAAA";         // String that will be the station ID
+		int indexCount = 0;            // Counter for array.
+		String[] strgArray = new String[5]; //readLine array
+		
+		// look for beginning of data, search for "STID"
+		while(!firstElement.contentEquals(STID_COLUMN)) {
+		strg = br.readLine();
+		strgArray = strg.split(" ");
+		firstElement = strgArray[1];
+		// System.out.println("The first element in: " + firstElement); // Show me whats stored HELPER
+		}
 				
-			}	
-			
-			br.close();
-		}
-		
-		
-		// Search through array looking for the index number for the station
-		public int indexOfStation() {
-			int i = 0;
-			while(!stations[i].contentEquals(MesoStation)) {
-				i++;
+		// Once STID is found, we can begin reading in the strings into an. array.
+		// We only want the 1st token of the string, the STID.
+		// while loops until the finalString is reached.
+		while (!index.contentEquals(finalstring)) {
+		strg = br.readLine();
+		strgArray = strg.split(" ");
+		index = strgArray[1];
+		stations[indexCount] = index;
+		// System.out.println(stations[indexCount]);            // Show me whats printed HELPER
+		indexCount++;	
+				
+		// creates an array +1 bigger than the original, when limit is reached.
+		if (indexCount == stations.length) {
+		String[] tempArray = new String[indexCount+1];
+		for (int i = 0; i < stations.length; i++) {
+			tempArray[i] = stations[i];
 			}
-			stationIndex = i + 1;
-			return stationIndex;
-			
+		stations = tempArray;
+		} 		
+		}	
+		br.close();
+	}
+		
+		
+	// Search through array looking for the index number for the station
+	public int indexOfStation() {
+		int i = 0;
+		while(!stations[i].contentEquals(MesoStation)) {
+			i++;
 		}
+		stationIndex = i + 1;
+		return stationIndex;		
+	}
 	//Calculate the +/- 1 and 2 for all indexes.
 	public void calculateNs(int stationIndex) {
-		// skip the first 2 and last 
-		// 0,1,2,3,......,117,118,119
-		// skip 0,1,118,119
-		
-		if (stationIndex == 0) {
-			
-			
-		}
-		
 		this.stationIndex = stationIndex;
 		
 		String nMinusOne = stations[stationIndex-2];
@@ -127,14 +113,5 @@ public class PosAvg {
 		System.out.println("Print n-2:" + nMinusTwo);
 		System.out.println("Print n+2:" + nPlusTwo);
 		
-		
-		// why isnt calculateNs working??
-
-		
-		/* for(int i = 2; i < 118; i++) {
-			//for n-1
-			String nMinusOne = stations[i-1];
-			String nPlusOne = stations[i+1];
-		} */
 	}
 }
